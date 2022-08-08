449ceb4d594ec4fa69db45d2467a376e468700a2
Mikhail <mikhailstepan1994@gmail.com>
2022-08-08 21:05:08 +0800

    First commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.hui, name='hui'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout')
 ]
